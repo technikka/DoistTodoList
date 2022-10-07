16bcc9ba95248b1c8b877e54bc4d15578dcd92c2
Pythonikk <nikkibrusoe@gmail.com>
2022-10-07 09:25:16 -0700

    Add todo item and form in html

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { todoItem } from './todo-item.js'
+
+const todoItems = [];
+
+let todo = todoItem('Odin TODO project', 'create a todo app', 'orange', Date.now, 'Odin');
---
+++ src/todo-item.js
@@ -0,0 +1,14 @@
+const todoItem = (title, description, priorityLevel, dueDate, category, notes) => {
+  title,
+  description,
+  priorityLevel,
+  dueDate,
+  category,
+  notes
+
+  const isComplete = false; 
+
+  return {title, description, priorityLevel, dueDate, category, notes }
+};
+
+export { todoItem }
