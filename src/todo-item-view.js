1be1279325d57f663941e5a51c3281d40c3aa817
Pythonikk <nikkibrusoe@gmail.com>
2022-10-14 13:24:38 -0700

    Add icons for delete and expand functions of todo item

--- src/todo-item-view.js
+++ src/todo-item-view.js
@@ -16,6 +16,39 @@ const _setColorByPriority = (element, priorityLevel) => {
   element.style.borderColor = priorityColor
 }
 
+const _todoContainerMinimized = (item) => {
+  let container = document.createElement('div');
+  container.classList.add('todo-item-container');
+
+  _setColorByPriority(container, item.priorityLevel);
+
+  // let deleteBtn = document.createElement('button');
+  let delIcon = document.createElement('img');
+  delIcon.src = '../src/assets/delete-forever.png';
+  // deleteBtn.style.backgroundImage = '../src/assets/delete-forever.png';
+  delIcon.classList.add('delete-icon');
+  container.appendChild(delIcon);
+
+  let title = document.createElement('span');
+  title.textContent = item.title;
+  container.appendChild(title);
+
+  let dueDate = document.createElement('span');
+  dueDate.textContent = item.dueDate;
+  container.appendChild(dueDate);
+
+  let category = document.createElement('span');
+  category.textContent = item.category;
+  container.appendChild(category);
+
+  let expandIcon = document.createElement('img');
+  expandIcon.src = '../src/assets/arrow-expand-down.png';
+  expandIcon.classList.add('expand-icon');
+  container.appendChild(expandIcon);
+
+  return container;
+}
+
 const showAllTodos = () => {
   const content = document.querySelector('#content');
   content.textContent = '';
@@ -23,27 +56,7 @@ const showAllTodos = () => {
   let items = getItems();
 
   items.forEach(item => {
-    let container = document.createElement('div');
-    container.classList.add('todo-item-container');
-    _setColorByPriority(container, item.priorityLevel);
-
-    let deleteBtn = document.createElement('button');
-    deleteBtn.textContent = "Del";
-    container.appendChild(deleteBtn);
-
-    let title = document.createElement('span');
-    title.textContent = item.title;
-    container.appendChild(title);
-
-    let dueDate = document.createElement('span');
-    dueDate.textContent = item.dueDate;
-    container.appendChild(dueDate);
-
-    let category = document.createElement('span');
-    category.textContent = item.category;
-    container.appendChild(category);
-
-    content.appendChild(container);
+    content.appendChild(_todoContainerMinimized(item));
   })
 }
 
